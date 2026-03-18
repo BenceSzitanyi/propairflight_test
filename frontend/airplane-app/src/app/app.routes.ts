2b980bec1aa70a2723eed6b4767c92df8151c230
BenceSzitanyi <szitka0830@gmail.com>
2026-03-18 17:28:00 +0100

    auth guard activated

--- frontend/airplane-app/src/app/app.routes.ts
+++ frontend/airplane-app/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import {AirplaneList} from './components/airplane-list/airplane-list';
 import { AirplaneDetailComponent } from './components/airplane-detail-component/airplane-detail-component';
 import { AirplaneAddComponent } from './components/airplane-add-component/airplane-add-component';
 import { LoginComponent } from './components/login-component/login-component';
+import {authGuard} from './guards/auth-guard';
 
 export const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  { path: 'airplanes', component: AirplaneList },
-  { path: 'airplanes/new', component: AirplaneAddComponent },
-  { path: 'airplanes/:id', component: AirplaneDetailComponent },
+  { path: 'login', component: LoginComponent},
+
+  { path: 'airplanes', component: AirplaneList, canActivate: [authGuard]},
+  { path: 'airplanes/new', component: AirplaneAddComponent, canActivate: [authGuard] },
+  { path: 'airplanes/:id', component: AirplaneDetailComponent, canActivate: [authGuard] },
 
 
   { path: '', redirectTo: 'airplanes', pathMatch: 'full' },
