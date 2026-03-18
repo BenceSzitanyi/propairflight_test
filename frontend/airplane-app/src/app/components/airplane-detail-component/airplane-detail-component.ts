4d97d011360bb1945f27e3eac42bc6a875bc03c4
BenceSzitanyi <szitka0830@gmail.com>
2026-03-18 21:55:54 +0100

    added detail component http fucntions

--- frontend/airplane-app/src/app/components/airplane-detail-component/airplane-detail-component.ts
+++ frontend/airplane-app/src/app/components/airplane-detail-component/airplane-detail-component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { AirplaneService } from '../../services/airplane-service';
 import {Airplane} from '../../models/airplane';
 import {TailNumberPipe} from '../../pipes/tail-number-pipe';
-import { NgClass } from '@angular/common';
+import { NgClass, CommonModule } from '@angular/common';
 import {MaintenanceBarComponent} from '../maintenance-bar-component/maintenance-bar-component';
 
 @Component({
@@ -17,10 +17,30 @@ export class AirplaneDetailComponent implements OnInit {
   private airplaneService = inject(AirplaneService);
 
   airplane: Airplane | undefined;
+  isLoading:boolean = false;
+  errorMessage:string = "";
 
   ngOnInit(): void {
-    const id = this.route.snapshot.params['id'];
-    this.airplane = this.airplaneService.getById(id);
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id){
+      this.fetchAirplaneDetails(id);
+    }
+  }
+
+  fetchAirplaneDetails(id: string):void{
+    this.isLoading = true;
+    this.errorMessage = '';
+    this.airplaneService.getById(id).subscribe({
+      next: (data) => {
+        this.airplane = data;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.isLoading = false;
+        this.errorMessage = 'Could not find this airplane. It might have been decommissioned.';
+        console.error('Detail Load Error:', error);
+      }
+    });
   }
 
   get maintenancePercentage(): number {
@@ -30,9 +50,9 @@ export class AirplaneDetailComponent implements OnInit {
     return Math.min(percent, 100);
   }
 
-  handleFlightAdded() {
+  /*handleFlightAdded() {
     if (this.airplane) {
       this.airplaneService.incrementFlights(this.airplane.id);
     }
-  }
+  }*/
 }
